5357b3a7f779088ee40ebc4db06f6b6561c756b8
unknown <emmco96@gmail.com>
2024-05-15 15:48:54 +0100

    new

--- min.php
+++ min.php
@@ -141,36 +141,36 @@ $result = $conn->query($sql);
     <div class="border-b border-gray-200 dark:border-gray-700" style="border-bottom: 1px solid #382968;" >
         <ul class="flex flex-wrap -mb-px text-sm font-medium text-center text-gray-500 dark:text-gray-400">
             <li class="mr-2">
-                <a href="#" onclick="handleNavClick('post')" class="inline-flex items-center justify-center p-4 border-b-2 border-transparent rounded-t-lg hover:text-[white] hover:border-[#FF595A] dark:hover:text-[#FF595A] group">
-                    <svg class="w-4 h-4 mr-2 text-gray-400 group-hover:text-gray-500 dark:text-gray-500 dark:group-hover:text-[#FF595A]" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                <a href="#" onclick="handleNavClick('post')" class="inline-flex items-center justify-center p-4 border-b-2 border-transparent rounded-t-lg hover:text-[white] hover:border-[#382968ec] dark:hover:text-[#382968ec] group">
+                    <svg class="w-4 h-4 mr-2 text-gray-400 group-hover:text-gray-500 dark:text-gray-500 dark:group-hover:text-[#382968ec]" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
                         <path d="M6.143 0H1.857A1.857 1.857 0 0 0 0 1.857v4.286C0 7.169.831 8 1.857 8h4.286A1.857 1.857 0 0 0 8 6.143V1.857A1.857 1.857 0 0 0 6.143 0Zm10 0h-4.286A1.857 1.857 0 0 0 10 1.857v4.286C10 7.169 10.831 8 11.857 8h4.286A1.857 1.857 0 0 0 18 6.143V1.857A1.857 1.857 0 0 0 16.143 0Zm-10 10H1.857A1.857 1.857 0 0 0 0 11.857v4.286C0 17.169.831 18 1.857 18h4.286A1.857 1.857 0 0 0 8 16.143v-4.286A1.857 1.857 0 0 0 6.143 10Zm10 0h-4.286A1.857 1.857 0 0 0 10 11.857v4.286c0 1.026.831 1.857 1.857 1.857h4.286A1.857 1.857 0 0 0 18 16.143v-4.286A1.857 1.857 0 0 0 16.143 10Z"/>
                     </svg>Make a Post
                 </a>
             </li>
             <li class="mr-2">
-                <a href="#" onclick="handleNavClick('pictures')" class="inline-flex items-center justify-center p-4 border-b-2 border-transparent rounded-t-lg hover:text-[white] hover:border-[#FF595A] dark:hover:text-[#FF595A] group">
-                    <svg class="w-4 h-4 mr-2 text-gray-400 group-hover:text-gray-500 dark:text-gray-500 dark:group-hover:text-[#FF595A]" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                <a href="#" onclick="handleNavClick('pictures')" class="inline-flex items-center justify-center p-4 border-b-2 border-transparent rounded-t-lg hover:text-[white] hover:border-[#382968ec] dark:hover:text-[#382968ec] group">
+                    <svg class="w-4 h-4 mr-2 text-gray-400 group-hover:text-gray-500 dark:text-gray-500 dark:group-hover:text-[#382968ec]" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
                         <path d="M10 0a10 10 0 1 0 10 10A10.011 10.011 0 0 0 10 0Zm0 5a3 3 0 1 1 0 6 3 3 0 0 1 0-6Zm0 13a8.949 8.949 0 0 1-4.951-1.488A3.987 3.987 0 0 1 9 13h2a3.987 3.987 0 0 1 3.951 3.512A8.949 8.949 0 0 1 10 18Z"/>
                     </svg>Upload Pictures
                 </a>
             </li>
             <li class="mr-2">
-                <a href="#" onclick="handleNavClick('delete')" class="inline-flex items-center justify-center p-4 border-b-2 border-transparent rounded-t-lg hover:text-[white] hover:border-[#FF595A] dark:hover:text-[#FF595A] group">
-                    <svg class="w-4 h-4 mr-2 text-gray-400 group-hover:text-gray-500 dark:text-gray-500 dark:group-hover:text-[#FF595A]" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                <a href="#" onclick="handleNavClick('delete')" class="inline-flex items-center justify-center p-4 border-b-2 border-transparent rounded-t-lg hover:text-[white] hover:border-[#382968ec] dark:hover:text-[#382968ec] group">
+                    <svg class="w-4 h-4 mr-2 text-gray-400 group-hover:text-gray-500 dark:text-gray-500 dark:group-hover:text-[#382968ec]" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
                         <path d="M16 1h-3.278A1.992 1.992 0 0 0 11 0H7a1.993 1.993 0 0 0-1.722 1H2a2 2 0 0 0-2 2v15a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2V3a2 2 0 0 0-2-2Zm-3 14H5a1 1 0 0 1 0-2h8a1 1 0 0 1 0 2Zm0-4H5a1 1 0 0 1 0-2h8a1 1 0 1 1 0 2Zm0-5H5a1 1 0 0 1 0-2h2V2h4v2h2a1 1 0 1 1 0 2Z"/>
                     </svg>Delete pictures/post
                 </a>
             </li>
             <li class="mr-2">
-                <a href="#" onclick="handleNavClick('certificate')" class="inline-flex items-center justify-center p-4 border-b-2 border-transparent rounded-t-lg hover:text-[white] hover:border-[#FF595A] dark:hover:text-[#FF595A] group">
-                    <svg class="w-4 h-4 mr-2 text-gray-400 group-hover:text-gray-500 dark:text-gray-500 dark:group-hover:text-[#FF595A]" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                <a href="#" onclick="handleNavClick('certificate')" class="inline-flex items-center justify-center p-4 border-b-2 border-transparent rounded-t-lg hover:text-[white] hover:border-[#382968ec] dark:hover:text-[#382968ec] group">
+                    <svg class="w-4 h-4 mr-2 text-gray-400 group-hover:text-gray-500 dark:text-gray-500 dark:group-hover:text-[#382968ec]" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
                         <path d="M5 11.424V1a1 1 0 1 0-2 0v10.424a3.228 3.228 0 0 0 0 6.152V19a1 1 0 1 0 2 0v-1.424a3.228 3.228 0 0 0 0-6.152ZM19.25 14.5A3.243 3.243 0 0 0 17 11.424V1a1 1 0 0 0-2 0v10.424a3.227 3.227 0 0 0 0 6.152V19a1 1 0 1 0 2 0v-1.424a3.243 3.243 0 0 0 2.25-3.076Zm-6-9A3.243 3.243 0 0 0 11 2.424V1a1 1 0 0 0-2 0v1.424a3.228 3.228 0 0 0 0 6.152V19a1 1 0 1 0 2 0V8.576A3.243 3.243 0 0 0 13.25 5.5Z"/>
                     </svg>Upload Certificate
                 </a>
             </li>
             <li class="mr-2">
-                <a href="#" onclick="handleNavClick('default')" class="inline-flex items-center justify-center p-4 border-b-2 border-transparent rounded-t-lg hover:text-[white] hover:border-[#FF595A] dark:hover:text-[#FF595A] group">
-                    <svg class="w-4 h-4 mr-2 text-gray-400 group-hover:text-gray-500 dark:text-gray-500 dark:group-hover:text-[#FF595A]" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                <a href="#" onclick="handleNavClick('default')" class="inline-flex items-center justify-center p-4 border-b-2 border-transparent rounded-t-lg hover:text-[white] hover:border-[#382968ec] dark:hover:text-[#382968ec] group">
+                    <svg class="w-4 h-4 mr-2 text-gray-400 group-hover:text-gray-500 dark:text-gray-500 dark:group-hover:text-[#382968ec]" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
                         <path d="M16 1h-3.278A1.992 1.992 0 0 0 11 0H7a1.993 1.993 0 0 0-1.722 1H2a2 2 0 0 0-2 2v15a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2V3a2 2 0 0 0-2-2Zm-3 14H5a1 1 0 0 1 0-2h8a1 1 0 0 1 0 2Zm0-4H5a1 1 0 0 1 0-2h8a1 1 0 1 1 0 2Zm0-5H5a1 1 0 0 1 0-2h2V2h4v2h2a1 1 0 1 1 0 2Z"/>
                     </svg>Student Database
                 </a>
@@ -225,7 +225,7 @@ $conn->close();
                             <label for="post" class="text-sm">Post description</label>
                             <textarea id="description" name="description" placeholder="" class="w-full rounded-md focus:ring focus:ri focus:ri dark:border-gray-700 dark:text-gray-900"></textarea>
                         </div>
-                        <button class="text-[white] bg-[#FF595A] border-0 py-2 px-6 focus:outline-none hover:bg-[#fe5000] rounded text-lg" type="submit">Delete</button>
+                        <button class="text-[white] bg-[#382968ec] border-0 py-2 px-6 focus:outline-none hover:bg-[#fe5000] rounded text-lg" type="submit">Delete</button>
                     </div>
                 </fieldset>
 </form> <br>
@@ -240,7 +240,7 @@ $conn->close();
                             <label for="post" class="text-sm">Post title</label>
                             <textarea id="description" name="title" placeholder="" class="w-full rounded-md focus:ring focus:ri focus:ri dark:border-gray-700 dark:text-gray-900"></textarea>
                         </div>
-                        <button class="text-[white] bg-[#FF595A] border-0 py-2 px-6 focus:outline-none hover:bg-[#fe5000] rounded text-lg" type="submit">Delete</button>
+                        <button class="text-[white] bg-[#382968ec] border-0 py-2 px-6 focus:outline-none hover:bg-[#fe5000] rounded text-lg" type="submit">Delete</button>
                     </div>
                 </fieldset>
             </form>
@@ -266,7 +266,7 @@ $conn->close();
                             <label for="file" class="text-sm">Attach a Photo (optional)</label>
                             <input type="file" name="file" class="w-full rounded-md focus:ring focus:ring focus:ring dark:border-gray-700 dark:text-gray-900"/>
                         </div>
-                        <button type='submit' class="text-[white] bg-[#FF595A] border-0 py-2 px-6 focus:outline-none hover:bg-[#fe5000] rounded text-lg">Upload</button>
+                        <button type='submit' class="text-[white] bg-[#382968ec] border-0 py-2 px-6 focus:outline-none hover:bg-[#fe5000] rounded text-lg">Upload</button>
                     </div>
                 </fieldset>
             </form>
@@ -289,7 +289,7 @@ $conn->close();
                             <label for="bio" class="text-sm">Upload Photo</label>
                             <input type="file" name="file" accept="image/*" required  class="w-full rounded-md focus:ring focus:ri focus:ri dark:border-gray-700 dark:text-gray-900" />
                         </div>
-                        <button class="text-[white] bg-[#FF595A] border-0 py-2 px-6 focus:outline-none hover:bg-[#fe5000] rounded text-lg" type="submit">Upload</button>
+                        <button class="text-[white] bg-[#382968ec] border-0 py-2 px-6 focus:outline-none hover:bg-[#fe5000] rounded text-lg" type="submit">Upload</button>
                     </div>
                 </fieldset>
             </form>
@@ -357,7 +357,7 @@ $conn->close();
                             <label for="country" class="text-sm">Skill learnt</label>
                             <input id="skill_learnt" type="text" name="skill_learnt" placeholder="" class="w-full rounded-md focus:ring focus:ri focus:ri dark:border-gray-700 dark:text-gray-900" />
                             <br/><br/>
-                            <button class="text-[white] bg-[#FF595A] border-0 py-2 px-6 focus:outline-none hover:bg-[#fe5000] rounded text-lg">Upload</button>
+                            <button class="text-[white] bg-[#382968ec] border-0 py-2 px-6 focus:outline-none hover:bg-[#fe5000] rounded text-lg">Upload</button>
                         </div> 
                     </div>
                 </fieldset>
